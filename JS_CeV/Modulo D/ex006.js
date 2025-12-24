6977cc696c0bba735fbea61fa64bd553d6ee5966
Arion Lauriano <camila@lauriano.org>
2025-12-24 12:42:50 -0300

    js

---
+++ JS_CeV/Modulo D/ex006.js
@@ -0,0 +1,7 @@
+var vel = 40;
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida de 60km/h. MULTADO!`);
+} else {
+    console.log(`Você está dentro do limite de velocidade. Dirija com segurança!`);
+}
