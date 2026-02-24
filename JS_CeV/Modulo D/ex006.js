594d2dcf84caed5ba9ae5bee083dff6ffeb76796
Camila R Lauriano <camila@lauriano.org>
2026-02-24 16:03:33 -0300

    Reorganizando

--- JS_CeV/Modulo D/ex006.js
+++
@@ -1,7 +0,0 @@
-var vel = 40;
-
-if (vel > 60) {
-    console.log(`Você ultrapassou a velocidade permitida de 60km/h. MULTADO!`);
-} else {
-    console.log(`Você está dentro do limite de velocidade. Dirija com segurança!`);
-}
